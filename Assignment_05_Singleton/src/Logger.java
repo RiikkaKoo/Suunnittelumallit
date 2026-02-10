89287e8f67d401aa361d1c2ada48989ccd5fd17b
riikka <riikaka@metropolia.fi>
2026-02-10 11:30:19 +0200

    Assignment 05: Added some comments and more "testing" to the main.

--- Assignment_05_Singleton/src/Logger.java
+++ Assignment_05_Singleton/src/Logger.java
@@ -6,6 +6,7 @@ public class Logger {
     private static Logger instance;
     private String filename;
 
+    // Set the default filename (but why?)
     private Logger() {
         this.filename = "log.txt";
     }
@@ -17,33 +18,39 @@ public class Logger {
         return instance;
     }
 
+    // A method for setting a new filename and closing the current file if it is open. Open/create new file and set it as the open file.
     public void setFileName(String filename) {
         try {
             this.filename = filename;
+            // Save and close the previous open file (if exists).
             if (this.writer != null) {
+                this.writer.flush();
                 this.writer.close();
             }
+            // Open/create new file:
             this.writer = new BufferedWriter(new FileWriter(this.filename, true)); // True = New lines are appended to an existing file.
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.out.println(e.getMessage());
         }
     }
 
+    // A method for writing a new line to the open file
     public void write(String text) {
-        try {
-            this.writer.write(text);
-            this.writer.newLine();
-            this.writer.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        if (this.writer != null) {
+            try {
+                this.writer.write(text);
+                this.writer.newLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {System.out.println("Logger is closed. Set filename to open it.");}
     }
 
     public void close() {
         try {
+            this.writer.flush();
             this.writer.close();
             this.writer = null;
-            instance = null;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- Assignment_05_Singleton/src/Main.java
+++ Assignment_05_Singleton/src/Main.java
@@ -1,13 +1,29 @@
 public class Main {
     public static void main(String[] args) {
         Logger logger = Logger.getInstance();
+
+        // Set filename to open the logger for writing
         logger.setFileName("new_log.txt");
-        logger.write("Simulation 2 started");
+        logger.write("Simulation started");
         logger.write("Processing data...");
-        logger.write("Simulation 2 finished");
+        logger.write("Simulation finished");
+
+        // Create a new file (since it does not exist)
         logger.setFileName("another_log.txt");
         logger.write("First line to a new file!");
         logger.write("Another line.");
+
+        // Close the file and try to write (should not work)
+        logger.close();
+
+        logger.write("THIS SHOULD NOT BE WRITTEN TO THE FILE!");
+
+        // Open the first file again. New lines should be appended after the existing ones.
+        logger.setFileName("new_log.txt");
+        logger.write("Second simulation started");
+        logger.write("Processing data...");
+        logger.write("Second simulation finished");
+
         logger.close();
     }
 }
